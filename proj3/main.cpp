892aee6f5f5000a879f70bdc95ea5d6a27003d79
sheputis <arnoldas@student.matnat.uio.no>
2015-10-21 19:02:26 +0200

    almost compeletely ready code

--- proj3/main.cpp
+++ proj3/main.cpp
@@ -29,15 +29,15 @@ using namespace std;
 double Legendre(int n, double x); // this is a function copied from lecture notes page 121:
 double int_function(double x1, double y1, double z1, double x2, double y2, double z2);
 double int_function_B(double phi_1, double phi_2, double th_1, double th_2, double r1, double r2);
-
+double int_function_D(double phi_1, double phi_2, double th_1, double th_2, double r1, double r2);
 
 int main()
 {
     //                                           PROBLEM A: SOLVED USING LEGENDRE
-    /*
+
     int N=30;
 
-    double a=-3;double b=3;
+    double a=-2.5;double b=2.5;
 
     //reserve memory for mesh points x and weights w
     double *x=new double[N];
@@ -62,8 +62,12 @@ int main()
 
     }
     //final output:
-    cout <<"Problem A, the integral using Legendre"<< int_gauss<<endl;
-     */
+    double exact= (5./(16.*16.))*M_PI*M_PI;
+    cout <<"Problem A, the integral using Legendre : "<< int_gauss<<endl;
+    cout <<" Number of points per dimension : " << N<<endl;
+    cout<< " Exact = " << exact <<endl;
+    cout<< " Relative error = " <<(int_gauss-exact)/exact;
+
     /*                                       PROBLEM B !!!!!!! Laguerre !!!!!!!!!!!!!!                   */
     /*
     int N= 5;
@@ -83,7 +87,7 @@ int main()
     //set up mesh points and weights:
     gauleg(a_th,b_th,x_th,w_th,N);
 
-//  mesh points for legendre -1 to 1: = angle phi
+//  mesh points for legendre -0 to 2pi : = angle phi
     double a_phi=0;double b_phi=2*M_PI;
 
     //reserve memory for mesh points x and weights w
@@ -113,8 +117,8 @@ int main()
     */
 
     //                                           PROBLEM C
-    /*
-    int n= 10000000;
+ /*
+    int n= 100000;
     long idum = -1;
     double int_mc=0; double x[6]; double sum_sigma=0;double fx; double variance=0.;
     double length=3.;double jacobidet=pow((2*length),6);
@@ -122,6 +126,7 @@ int main()
     for (int i= 1;i<=n;i++){
         for(int j=0;j<6;j++){
             x[j]=-length+2*length*ran0(&idum);
+
 
         }
         fx=int_function(x[0],x[1],x[2],x[3],x[4],x[5]);
@@ -139,7 +144,7 @@ int main()
     cout<< " Exact = " << (5./(16.*16.))*M_PI*M_PI<<endl;
     cout<< " Sigma = " << setw(10)<< setprecision(8)<< jacobidet*sqrt(variance/((double) n))<<endl;
 
-    */
+ */
 
 
 
@@ -148,7 +153,44 @@ int main()
 
 
     //                                           PROBLEM D
+/*
+    int n= 1000000;
+    long idum = -1;
+    double int_mc=0; double x[6]; double sum_sigma=0;double fx; double variance=0.;
+    double jacobidet=(M_PI*M_PI)/(pow(4.,3));
+    //evaluating integral with the crude Montecarlo method:
+    for (int i= 1;i<=n;i++){
 
+        x[0]=ran0(&idum);  // this is x1 [0,1] converted from r1 [0,inf)
+        x[1]=ran0(&idum);  // x2
+        //converting to y:
+        x[0]=-log(1-x[0]);
+        x[1]=-log(1-x[1]);
+
+        x[2]=-1+2*ran0(&idum); //dcos_theta_1
+        x[3]=-1+2*ran0(&idum); //dcos_theta_2
+        x[4]=2*M_PI*ran0(&idum); //d_phi_1
+        x[5]=2*M_PI*ran0(&idum); //d_phi_2
+
+      //  cout<<x[0]<<endl;
+
+
+        fx=int_function_D(x[4],x[5],x[2],x[3],x[0],x[1]);
+      //  fx=int_function_B(x[0],x[1],x[2],x[3],x[4],x[5]);
+        int_mc+=fx;
+        sum_sigma +=fx*fx;
+
+    }
+    int_mc = int_mc/((double) n);
+    sum_sigma = sum_sigma/((double) n);
+    variance=sum_sigma-int_mc*int_mc;
+
+    //final output:
+    cout<< setiosflags(ios::showpoint | ios::uppercase);
+    cout<< " Monte carlo result " << setw(10) << setprecision(8)<< jacobidet*int_mc <<endl;
+    cout<< " Exact = " << (5./(16.*16.))*M_PI*M_PI<<endl;
+    cout<< " Sigma = " << setw(10)<< setprecision(8)<< jacobidet*sqrt(variance/((double) n))<<endl;
+ */
 
     return 0;
 }
@@ -166,13 +208,13 @@ double int_function(double x1,double y1, double z1, double x2, double y2, double
     double value;
     if(denominator < pow(10.,-6.))
     {
-       // cout<<"B";
+
         value=0;
 
     }
     else
     {
-        //cout<< "A";
+
         value=numerator/denominator;
     }
     return value;
@@ -199,12 +241,24 @@ double int_function_B(double phi_1, double phi_2, double th_1, double th_2, doub
 
 
 
+double int_function_D(double phi_1, double phi_2, double th_1, double th_2, double r1, double r2)
+{
+
+    double numerator=(r1*r1)*(r2*r2);
+
+    double cos_Bet;
+    cos_Bet=th_1*th_2+abs(sqrt(1-th_1*th_1))*abs(sqrt(1-th_2*th_2))*cos(phi_1-phi_2);
+    double denominator=sqrt(r1*r1+r2*r2-2*r1*r2*cos_Bet);
+    double value;
+    if(denominator< pow(10.,-6))
+    {
+        value =0;//cout<< r12<< " " << (r12< pow(10.,-6))<< " "<< pow(10.,-6)<<endl;
 
 
-
-
-
-
+    }
+    else{value=numerator/denominator;}
+    return value;
+}
 
 
 
